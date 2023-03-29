a44d171cf9e1687c9e49cfc2b1330e02f2774436
EC2 Default User <ec2-user@ip-172-31-5-80.us-east-2.compute.internal>
2023-03-29 17:59:36 +0000

    Flexbox <3

--- .vitepress/theme/components/ContentPage.vue
+++ .vitepress/theme/components/ContentPage.vue
@@ -17,7 +17,7 @@ const { path } = useRoute();
         <Content />
         <div class="content-meta">
           <div class="edit"><a href="#">
-              <svg data-v-2d8bc1e6="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="edit-link-icon"
+              <svg data-v-2d8bc1e6="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="edit-icon"
                 aria-label="edit icon">
                 <path
                   d="M18,23H4c-1.7,0-3-1.3-3-3V6c0-1.7,1.3-3,3-3h7c0.6,0,1,0.4,1,1s-0.4,1-1,1H4C3.4,5,3,5.4,3,6v14c0,0.6,0.4,1,1,1h14c0.6,0,1-0.4,1-1v-7c0-0.6,0.4-1,1-1s1,0.4,1,1v7C21,21.7,19.7,23,18,23z">
@@ -115,19 +115,20 @@ table {
 .content-meta {
   font-size: .75em;
   margin-top: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 14px;
 }
 
 .content-meta .edit {
-  float: left;
+  display: flex;
+  align-items: center;
 }
 
-.edit-link-icon {
+.edit-icon {
   width: 20px;
   vertical-align: middle;
   margin-right: 5px;
-}
-
-.content-meta .content-date {
-  float: right;
 }
 </style>
